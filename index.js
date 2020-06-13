8fab01ef81b43dd158954bb403deb1b6b3ddc037
Sumit <40204694+sum1112@users.noreply.github.com>
2020-06-13 11:46:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+var numberOfDrums = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrums; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    makeAnimation(this.innerHTML);
+    makePlay(this.innerHTML);
+
+  });
+}
+
+document.addEventListener("keypress", function(event) {
+  var str = event.key;
+  makeAnimation(str);
+  makePlay(str);
+});
+
+function makeAnimation(key) {
+
+  var buttonPressed = document.querySelector("." + key);
+  buttonPressed.classList.add('pressed');
+
+  setTimeout(function (){
+    buttonPressed.classList.remove('pressed');
+  }, 100);
+
+}
+
+function makePlay(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "l":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    default:
+
+  }
+}
